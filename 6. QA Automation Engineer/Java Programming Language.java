f0bc4a92ed7b769e1bcc32ad43f2804741e8505f
VadimKoop <tallinn121@gmail.com>
2024-01-27 12:18:16 +0200

    files relocation

---
+++ 6. QA Automation Engineer/Java Programming Language.java
@@ -0,0 +1,14 @@
+//Java
+Java Programming language has static typing. Java has lowerCamelCase style + SCREAMING_SNAKE_FORCE.
+Java Programming language has primitive and reference types.
+
+
+//Table
+Name:	Size:	Minimum value
+int		32		-2147483648
+byte	8		-128
+short	16		-32768
+long	64		-922372036854775808
+char	16
+float	32
+double	64
