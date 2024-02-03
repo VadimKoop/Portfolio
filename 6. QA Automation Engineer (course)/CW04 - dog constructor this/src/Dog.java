b7ad8de6c6cce62758ad85c3c9d52e5816e2808e
VadimKoop <tallinn121@gmail.com>
2024-02-03 13:17:10 +0200

    CW04

---
+++ 6. QA Automation Engineer (course)/CW04 - dog constructor this/src/Dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+
+    String breed;
+
+    double weight;
+
+    public Dog() {
+        this.breed = "Lambakoer";
+    }
+
+    public Dog(String breed) {
+        this.breed = breed;
+    }
+    
+}
---
+++ 6. QA Automation Engineer (course)/CW04 - dog constructor this/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog("Superkoer");
+
+        System.out.println(dog1.breed);
+
+        }
+    }
