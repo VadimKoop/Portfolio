d4fc4b0d556751c2b9d9b387b591ab77d99b1e7b
VadimKoop <tallinn121@gmail.com>
2024-02-06 19:38:20 +0200

    HW04 - figure constructor, methods (comments)

--- 6. QA Automation Engineer (course)/HW04 - figure constructor this/src/Figure.java
+++ 6. QA Automation Engineer (course)/HW04 - figure constructor this/src/Figure.java
@@ -1,5 +1,4 @@
 public class Figure {
-
     int height;
     int width;
     int length;
@@ -16,26 +15,27 @@ public class Figure {
 
     // Creating method which calculates volume of the figure.
     public double calculateVolume() {
-        System.out.println("Class calculateVolume, the sum of figure sizes is: " + this.height + this.width + this.length); /** Спросить про момент про сложение чисел*/
-        System.out.println(this.height + this.width + this.length);
+        System.out.println("Class calculateVolume, the sum of figure sizes is: " + this.height + this.width + this.length);
+        System.out.println(this.height + this.width + this.length); // -> 10.
         return this.length * this.width * this.height;
     }
 
     // A method that calculates the surface area of figure from formula S = 2(ab + ah + bh).
     public void calculateSurfaceArea() {
         double surfaceArea = 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
-        System.out.println("Surface area of the figure: " + surfaceArea);
+        System.out.println("Surface area of the figure: " + surfaceArea); // -> 62.0
     }
 
 
     public static void main(String[] args) {
         // Create an object of the Figure class, giving height, height and length values.
         Figure figureSizes = new Figure (5, 3, 2);
-        System.out.println("Figure sizes are: " + figureSizes);
+        System.out.println("Figure sizes are: " + figureSizes); // -> Figure@3feba861
+        System.out.println("Figure sizes are: " + figureSizes.height + figureSizes.width + figureSizes.length); // -> 532
 
         // Creating double volume variable and calling 2 methods.
         double volume = figureSizes.calculateVolume();
-        System.out.println("Figure volume is: " + volume);
+        System.out.println("Figure volume is: " + volume); // -> 30.0
 
         figureSizes.calculateSurfaceArea();
     }
