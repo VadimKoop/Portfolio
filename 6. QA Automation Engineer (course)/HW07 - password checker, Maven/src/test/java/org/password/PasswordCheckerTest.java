e398b45c24f15b1ffb3dcb7da886fd268c84a517
VadimKoop <tallinn121@gmail.com>
2024-02-17 11:57:24 +0200

    Manual updates

--- 6. QA Automation Engineer (course)/HW07 - password checker, Maven/src/test/java/org/password/PasswordCheckerTest.java
+++ 6. QA Automation Engineer (course)/HW07 - password checker, Maven/src/test/java/org/password/PasswordCheckerTest.java
@@ -9,7 +9,7 @@ public class PasswordCheckerTest {
     @Test
     void shouldReturnTrueForComplexPasswordWithDigitsAndSpecialChars() {
         assertTrue(PasswordChecker.checkPasswordComplexity("Password123!"));
-
+/*
         @Before
         public void setUp () {
             System.setIn(sysInBackup);
@@ -47,6 +47,6 @@ public class PasswordCheckerTest {
         private void provideInput (String data){
             ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
             System.setIn(testIn);
-        }
+        } */
     }
-    }
+}
