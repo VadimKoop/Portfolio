3856b487acb6db9282e9a03ce16abbf1290048b5
VadimKoop <tallinn121@gmail.com>
2024-02-25 23:25:18 +0200

    Reverse HW09 commit.

--- 6. QA Automation Engineer (course)/HW07 - password checker, Maven, Rest Assured/src/test/java/api/RestApiMocked.java
+++ 6. QA Automation Engineer (course)/HW07 - password checker, Maven, Rest Assured/src/test/java/api/RestApiMocked.java
@@ -29,43 +29,7 @@ public class RestApiMocked {
                 .statusCode(200); // 200 - OK.
     }
 
-    /** Hw09.*/
 
-    @Test
-    public void deleteValidOrderAndResponseIsOk() {
-        //Retrieve details of an order by providing a valid order ID from 1..10. Other ID will lead to 404 Request Code.
-        given()
-                .log()
-                .all()
-
-                .when()
-                .header("api_key", "1234567890123456") // s - String, o - Object.
-                .delete("http://35.208.34.242:8080/test-orders/9")
-
-
-                .then()
-                .log()
-                .all()
-                .statusCode(204); // HTTP Status 204 (No Content) indicates that the server has successfully fulfilled the request and that there is no content to send in the response payload body
-    }
-
-    @Test
-    public void deleteWrongKeyOrderAndResponseIsNok() {
-        //Retrieve details of an order by providing a valid order ID from 1..10. Other ID will lead to 404 Request Code.
-        given()
-                .log()
-                .all()
-
-                .when()
-                .header("wrong_key", "101")
-                .delete("http://35.208.34.242:8080/test-orders/9")
-
-
-                .then()
-                .log()
-                .all()
-                .statusCode(204); // HTTP Status 204 (No Content) indicates that the server has successfully fulfilled the request and that there is no content to send in the response payload body
-    }
 }
 
 //Source: http://35.208.34.242:8080/swagger-ui/index.html#/Mocked%20Order%20Operations/getById
