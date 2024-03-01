c9f94937d2611e58f5b607bb2545e818f33ddce9
VadimKoop <tallinn121@gmail.com>
2024-03-01 23:05:59 +0200

    CW10 code fix

--- 6. QA Automation Engineer (course)/HW07 - password checker, Maven, Rest Assured/src/test/java/api/RestApiMocked.java
+++ 6. QA Automation Engineer (course)/HW07 - password checker, Maven, Rest Assured/src/test/java/api/RestApiMocked.java
@@ -1,15 +1,33 @@
 package api;
 
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static io.restassured.RestAssured.*;
 
 public class RestApiMocked {
 
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "http://35.208.34.242";
+        RestAssured.port = 8080;
+    }
+
     @Test
-    public void getOrderByIDAndCheckResponseCodeIsOK() {
-        //Retrieve details of an order by providing a valid order ID from 1..10. Other ID will lead to 404 Request Code.
-        get("http://35.208.34.242:8080/test-orders/9") //
+    public void getOrderByIDAndCheckResponseCodeIsOK() {  ////Retrieve details of an order by providing a valid order ID from 1..10. Other ID will lead to 404 Request Code.
+        given().
+                log()
+                .all()
+                .when()
+                .get("http://35.208.34.242:8080/test-orders/9") //
                 .then()
+                .log()
+                .all()
                 .statusCode(200); // 200 - OK.
     }
 
@@ -29,19 +47,18 @@ public class RestApiMocked {
                 .statusCode(200); // 200 - OK.
     }
 
-    /** HW09.*/
+    /**
+     * HW09.
+     */
     @Test
     public void deleteValidOrderAndResponseIsOk() {
         //Retrieve details of an order by providing a valid order ID from 1..10. Other ID will lead to 404 Request Code.
         given()
                 .log()
                 .all()
-
                 .when()
                 .header("api_key", "1234567890123456") // s - String, o - Object.
                 .delete("http://35.208.34.242:8080/test-orders/9")
-
-
                 .then()
                 .log()
                 .all()
@@ -52,19 +69,63 @@ public class RestApiMocked {
     public void deleteWrongKeyOrderAndResponseIsNok() {
         //Retrieve details of an order by providing a valid order ID from 1..10. Other ID will lead to 404 Request Code.
         given()
-                .log()
-                .all()
-
                 .when()
-                .header("wrong_key", "101")
-                .delete("http://35.208.34.242:8080/test-orders/9")
-
-
+                .get("http://35.208.34.242:8080/test-orders/11")
                 .then()
                 .log()
                 .all()
-                .statusCode(204); // HTTP Status 204 (No Content) indicates that the server has successfully fulfilled the request and that there is no content to send in the response payload body
+                .statusCode(400); // HTTP Status Bad Request error (No Content) indicates that the server has successfully fulfilled the request and that there is no content to send in the response payload body
     }
+
+    /**
+     * CW10
+     */
+    //@Test
+    @RepeatedTest(11)
+    public void deleteOrder() {
+        RestAssured
+                .given()
+                .log()
+                .all()
+                .when()
+                .header("api_key", "1234567890123456")
+                .delete("http://35.208.34.242:8080/test-orders/1")
+                .then()
+                .log()
+                .all()
+                .statusCode(204);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 9, 10, 11})
+    public void getOrdersByIdAndCheckResponseCodeIsOk(int orderId) {
+        given().
+                log()
+                .all()
+                .when()
+                .get("/test-orders/" + orderId) //Retrieve details of an order by providing a valid order ID from 1..10. Other ID will lead to 404 RC
+//                .get("/test-orders/{orderId}", orderId)  // Alternative method how to send parameter.
+//                .get("/test-orders/user/{userid}/{orderId}", orderId)  // Alternative method how to send parameter.
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {7, 8})
+    public void getOrdersByIdAndCheckResponseCodeIsNok(int orderId) {
+        given().
+                log()
+                .all()
+                .when()
+                .get("/test-orders/" + orderId) //Retrieve details of an order by providing a valid order ID from 1..10. Other ID will lead to 404 RC
+                .then()
+                .log()
+                .all()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
 }
 
 //Source: http://35.208.34.242:8080/swagger-ui/index.html#/Mocked%20Order%20Operations/getById
