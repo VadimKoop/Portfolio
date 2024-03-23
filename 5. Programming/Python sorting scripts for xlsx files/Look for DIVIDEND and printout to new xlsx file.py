2db582cb4ab5f45289410325adf07ee02a956cd4
VadimKoop <tallinn121@gmail.com>
2024-03-23 20:50:57 +0200

    Look for DIVIDEND and printout to new xlsx file

---
+++ 5. Programming/Python sorting scripts for xlsx files/Look for DIVIDEND and printout to new xlsx file.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# Loading data from Excel file.
+file_path = r"C:\Users\talli\Desktop\Revolut\PutNameHere.xlsx" #Put correct path to file.
+df = pd.read_excel(file_path)
+
+# Converting date strings to datetime format.
+df['Date'] = pd.to_datetime(df['Date']).dt.tz_localize(None)  # Removing time zone information.
+
+# We convert the date to the following format "year, month, day".
+df['Date'] = pd.to_datetime(df['Date']).dt.strftime('%Y-%m-%d')
+
+# Removing 'Quantity' and 'Price per share' columns.
+df.drop(columns=['Quantity', 'Price per share'], inplace=True)
+
+# Filtering rows named "DIVIDEND".
+dividend_transactions = df[df['Type'].str.contains("DIVIDEND", case=False)]
+
+# Saving filtered data to a new Excel file.
+output_file_path = r"C:\Users\talli\Desktop\Revolut\PutNameHere.xlsx" #Put correct path to file.
+dividend_transactions.to_excel(output_file_path, index=False)
+
+print("Rows named 'DIVIDEND' were saved in a new Excel file:", output_file_path)
