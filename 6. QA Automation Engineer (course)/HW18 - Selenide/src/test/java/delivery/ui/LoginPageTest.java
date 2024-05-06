d597717a093d07f2d025b657bac0276ffbe571b3
VadimKoop <tallinn121@gmail.com>
2024-05-06 23:31:04 +0300

    HW18 - Selenide and Xpath

--- 6. QA Automation Engineer (course)/HW18 - Selenide/src/test/java/delivery/ui/LoginPageTest.java
+++ 6. QA Automation Engineer (course)/HW18 - Selenide/src/test/java/delivery/ui/LoginPageTest.java
@@ -69,7 +69,48 @@ public class LoginPageTest {
 
     }
 
+    /* HW 18. */
 
+    @Test
+    public void LoginWithIncorrectDataAndImplementErrorChecking() {
+
+        $x("//*[@data-name=\"username-input\"]").sendKeys("WrongName");
+        $x("//*[@data-name=\"password-input\"]").sendKeys("WrongPassword");
+        $x("//*[@data-name=\"signIn-button\"]").click();
+        $x("//*[@data-name=\"authorizationError-popup-close-button\"]").shouldBe(Condition.visible);
+    }
+
+    @Test
+    public void LoginWithTheCorrectDataAndCheckOrderPage() {
+
+        $x("//*[@data-name=\"username-input\"]").sendKeys("vadimkp");
+        $x("//*[@data-name=\"password-input\"]").sendKeys("pd2V7wYbfT2n");
+        $x("//*[@data-name=\"signIn-button\"]").click();
+        $x("//*[@data-name=\"authorizationError-popup-close-button\"]").shouldBe(Condition.visible);
+        $x("//*[@data-name=\"createOrder-button\"]").shouldBe(Condition.visible);
+        $x("//*[@data-name=\"logout-button\"]").click();
+    }
+
+
+    // Error checking using a minimum number of characters (2 scenario username + password) - try to find tricky selector
+    @Test
+    public void TwoScenarioUsernameAndPassword() {
+
+        // Selector for username input field
+        $x("//input[contains(@data-name,'username') and contains(@data-name,'input') and not(contains(@data-name,'password'))]").
+                sendKeys("vadimkp");
+
+        // Selector for password input field
+        $x("//input[contains(@data-name,'password') and contains(@data-name,'input') and not(contains(@data-name,'username'))]").
+                sendKeys("pd2V7wYbfT2n");
+
+        // Selector for sign-in button
+        $x("//button[contains(@data-name,'signIn')]").click();
+
+        // Selector for authorization error popup close button
+        $x("//button[contains(@data-name,'authorizationError') and contains(@data-name,'popup-close-button')]").
+                shouldBe(Condition.visible);
+    }
 
 
 }
